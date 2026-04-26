df0bc26b1f663105680d51e5373e0ca2da01de80
Barathparthi <barathparthi2005kumar@gmail.com>
2026-04-26 11:41:32 +0530

    servers.json, .sh files removed

--- deploy_bundle/isolate_run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Remove --cg from arguments
-args=()
-for arg in "$@"; do
-  if [[ "$arg" != "--cg" ]]; then
-    args+=("$arg")
-  fi
-done
-exec /usr/local/bin/isolate_run_original "${args[@]}"
